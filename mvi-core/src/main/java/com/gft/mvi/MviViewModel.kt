30aac9f495d0df49fdea60ff5c10e8cec21238b2
Krzysztof Lopacki <krzysztof.lopacki@gft.com>
2024-05-23 09:43:48 +0200

    Correct MutableState to MutableStateFlow in error message in MviViewModel

--- mvi-core/src/main/java/com/gft/mvi/MviViewModel.kt
+++ mvi-core/src/main/java/com/gft/mvi/MviViewModel.kt
@@ -12,22 +12,22 @@ interface MviViewModel<VS : ViewState, EV : ViewEvent, NE : NavigationEffect, VE
 
     fun MviViewModel<VS, EV, NE, VE>.dispatchNavigationEffect(effect: NE) {
         if (navigationEffects is MutableStateFlow) (navigationEffects as MutableStateFlow).value = ConsumableEvent(effect)
-        else throw TypeCastException("MviViewModel.dispatchNavigationEffect extension supports MutableState only!")
+        else throw TypeCastException("MviViewModel.dispatchNavigationEffect extension supports MutableStateFlow only!")
     }
 
     fun MviViewModel<VS, EV, NE, VE>.clearNavigationEffect() {
         if (navigationEffects is MutableStateFlow) (navigationEffects as MutableStateFlow).value = null
-        else throw TypeCastException("MviViewModel.clearNavigationEffect extension supports MutableState only!")
+        else throw TypeCastException("MviViewModel.clearNavigationEffect extension supports MutableStateFlow only!")
     }
 
     fun MviViewModel<VS, EV, NE, VE>.dispatchViewEffect(effect: VE) {
         if (viewEffects is MutableStateFlow) (viewEffects as MutableStateFlow).value = ConsumableEvent(effect)
-        else throw TypeCastException("MviViewModel.dispatchViewEffect extension supports MutableState only!")
+        else throw TypeCastException("MviViewModel.dispatchViewEffect extension supports MutableStateFlow only!")
     }
 
     fun MviViewModel<VS, EV, NE, VE>.clearViewEffect(effect: VE) {
         if (viewEffects is MutableStateFlow) (viewEffects as MutableStateFlow).value = null
-        else throw TypeCastException("MviViewModel.clearViewEffect extension supports MutableState only!")
+        else throw TypeCastException("MviViewModel.clearViewEffect extension supports MutableStateFlow only!")
     }
 
     var MviViewModel<VS, EV, NE, VE>.viewState: VS
@@ -36,7 +36,7 @@ interface MviViewModel<VS : ViewState, EV : ViewEvent, NE : NavigationEffect, VE
         }
         set(value) {
             if (viewStates is MutableStateFlow) (viewStates as MutableStateFlow).value = value
-            else throw TypeCastException("MviViewModel.viewState extension supports MutableState only!")
+            else throw TypeCastException("MviViewModel.viewState extension supports MutableStateFlow only!")
         }
 }
 
